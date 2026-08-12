0c7a06ec73872a37230894021eca312c66f50f32
Lorenna Aguiar Nunes <lorenna.nunes@mpce.mp.br>
2026-08-12 14:12:08 -0300

    first added modal window practice + starter pig game files

--- Section 7 - JS in the Browser/Modal Window/script.js
+++ Section 7 - JS in the Browser/Modal Window/script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const btncloseModal = document.querySelector('.close-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  overlay.classList.remove('hidden');
+  modal.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  overlay.classList.add('hidden');
+  modal.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+btncloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
